79ca9c1149969ff205a54c532c0a3b7968a57b7f
miish <m.bogdanov@netpeak.net>
2018-03-02 19:41:15 +0200

    initial commit

---
+++ test-app/src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        console.log('items', this.props.items);
+        return (
+            <div>
+                Header
+            </div>
+        );
+    }
+}
+
+export default Header;
